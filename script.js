8698041490574f9e82928c242f143a65e45cda15
Naveen Kumar <naveenkumar139117@gmail.com>
2026-07-21 17:15:29 +0530

    Real Estate Agenc

---
+++ script.js
@@ -0,0 +1,42 @@
+ 
+window.addEventListener("scroll", () => {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+ 
+const hamburger = document.querySelector(".hamburger");
+const navWrapper = document.querySelector(".nav-wrapper");
+const body = document.body;
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navWrapper.classList.toggle("active");
+    
+ 
+    body.classList.toggle("no-scroll");
+});
+
+ 
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(e) {
+ 
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        
+ 
+        this.classList.add('active');
+
+ 
+        if (window.innerWidth <= 768) {
+            hamburger.classList.remove("active");
+            navWrapper.classList.remove("active");
+            body.classList.remove("no-scroll");
+        }
+    });
+});
